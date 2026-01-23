e2ae5827587ab6d5dff96ee4aa2f04e46d0fd569
Ashish <kapoorashish713@gmail.com>
2026-01-23 12:40:17 +0530

    adding premium button for mobile

--- client/src/components/layout/bottom-navbar.jsx
+++ client/src/components/layout/bottom-navbar.jsx
@@ -1,13 +1,19 @@
 // src/components/BottomNavbar.jsx
 import { NavLink } from "react-router-dom";
-import { Home, Search, User, Plus, Inbox } from "lucide-react";
-
+import { Home, Search, User, Inbox, Diamond } from "lucide-react";
+import { useLocation } from "react-router-dom";
 export default function BottomNavbar() {
+  const location = useLocation();
+  const isPremium = location.pathname === "/premium";
   return (
     <>
-      <button className="md:hidden fixed bottom-20 right-4 bg-[#1DA1F2] p-4 rounded-full">
-        <Plus />
-      </button>
+      {!isPremium && (
+        <button className="md:hidden fixed bottom-20 right-4 bg-[#1DA1F2] p-4 rounded-full">
+          <NavLink to="/premium">
+            <Diamond />
+          </NavLink>
+        </button>
+      )}
 
       <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-black border-t border-gray-800 flex justify-around py-2">
         <NavLink to="/">
@@ -19,7 +25,7 @@ export default function BottomNavbar() {
         <NavLink to="/profile">
           <User />
         </NavLink>
-         <NavLink to="/inbox">
+        <NavLink to="/inbox">
           <Inbox />
         </NavLink>
       </nav>
