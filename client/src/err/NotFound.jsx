09edae5cf47009178d017393914a3ba53d4966db
Ashish <kapoorashish713@gmail.com>
2026-01-24 15:33:02 +0530

    fixing notfound page

--- client/src/err/NotFound.jsx
+++ client/src/err/NotFound.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-
+import { NavLink } from "react-router-dom";
 const NotFound = () => {
   return (
-    <div>
-      NotFound
+    <div className='bg-black w-full  h-screen flex flex-col justify-center items-center text-white'>
+      <p className='animate-pulse text-4xl duration-75 transition-all font-bold'>404</p>
+      <p className='animate-pulse text-4xl duration-75 transition-all font-bold'>NotFound</p>
+      <NavLink className={"text-sm text-neutral-100 underline underline-offset-2"} to="/login">
+Go back
+      </NavLink>
     </div>
   );
 }
